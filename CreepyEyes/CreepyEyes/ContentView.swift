8abe4bf58b220f750b1e5c02dbdf0afb67b56b4f
Javier Arroyo <javiera@arroyoc.com>
2026-09-06 04:10:34 -0700

    Initialize CreepyEyes Xcode project

---
+++ CreepyEyes/CreepyEyes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreepyEyes
+//
+//  Created by Javier Arroyo on 9/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreepyEyes/CreepyEyes/CreepyEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreepyEyesApp.swift
+//  CreepyEyes
+//
+//  Created by Javier Arroyo on 9/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CreepyEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
